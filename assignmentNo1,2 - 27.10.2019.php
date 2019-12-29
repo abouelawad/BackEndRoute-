15a6022880d860b5bf7a1a14d227742c84fdd934
abouelawad <35901393+abouelawad@users.noreply.github.com>
2019-12-29 21:04:39 +0200

    Add files via upload

---
+++ assignmentNo1,2 - 27.10.2019.php
@@ -0,0 +1,274 @@
+
+
+
+ <p> Assignment 1</p>
+
+<hr>
+
+
+<?php
+
+echo "<p>1- Days Translator </p><br/>";
+
+$day="الاحد";
+
+if ($day == "السبت") {
+ echo "Saturday<br/>";
+}else if ($day == "الاحد") {
+ echo "Sunday<br/>";
+}else if ($day == "الاثنين") {
+ echo "Monday<br/>";
+}else if ($day == "الثلاثاء") {
+ echo "Tuseday<br/>";
+}else if ($day == "الاربعاء") {
+ echo "Wednesday<br/>";
+}else if ($day == "الخميس") {
+ echo "Thurusday<br/>";
+}else if ($day == "الجمعة") {
+ echo "Friday<br/>";
+} else {
+ echo "Not A Day<br/>";
+}
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>2-Login Page</p><br/>";
+
+$userName="ahrmed";
+$passWord="123";
+
+echo "userName type is : ".gettype($userName)."<br/>";
+echo "passWord type is : ". gettype($passWord)."<br/>";
+
+if($userName == "ahmed" && $passWord == "123"){
+	echo "Access Approved <br/>";
+}else {
+	echo "Access Denied <br/>";
+}
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>3 - Tester</p><br/>";
+
+$tester = false;
+
+
+if(gettype($tester) == "string"){
+	echo "Tester is a ".gettype($tester);
+}else if (gettype($tester) == "boolean") {
+//onther if for echo true or false
+	echo "Tester is a ".gettype($tester);
+	if($tester == true){
+		echo " type <b>true</b>";
+	}else{
+		echo " type <b>false</b>";
+	}
+}else if (gettype($tester) == "integer") {
+	echo "Tester is a ".gettype($tester);
+	echo " with a value = ";
+	echo $tester +5 ;
+}elseif (gettype($tester) == "double") {
+	echo "Tester is a ".gettype($tester);
+	echo " with a value = ";
+	echo $tester *7 ;
+
+}else{
+	echo "lef w erga3 tany";
+}
+
+
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>4 - movie</p><br/>";
+
+$films = array("Fast", "Predestination", "Persuit", "Prestige");
+$keyword = "Persuit";
+
+for($i = 0 ; $i < count($films) ; $i++ ){
+	if ($films[$i] == $keyword) {
+		echo "yes <br/>";
+		break;
+		
+	 }else{
+		echo "no <br/>";
+		
+	}
+	
+}
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>5 - Counting </p><br/>";
+
+$films=array("avatar","Prestige","avatar","Prestige");
+$keyword="avatar";
+$total=0;
+
+foreach ($films as $repeat) {
+	if ($repeat == $keyword) {
+
+		$total ++ ;
+	}
+}
+echo "Number of repating the movie avatar is ",$total,"<br/>" ;
+
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>6 - reverse </p><br/>";
+
+ 
+$films=array("avatar","Prestige","avatar","Prestige");
+
+for ($i = count($films) -1 ; $i >= 0 ; $i--) { 
+	echo $films[$i]." - ";
+}
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>7 - Boolean </p><br/>";
+
+echo "<p>Solution with 'for' loop</p><br/>";
+
+$tests=array(1,"tariq",1.5,true,7,'s',false);
+
+for ($i=0; $i < count($tests) ; $i++) { 
+	
+	if (gettype($tests[$i]) != 'boolean' ) {
+		echo gettype($tests[$i]),"<br/>";
+	}else{
+
+		if ($tests[$i] != true){
+		echo "<b>false</b><br/>";
+		}else {
+		echo "<b>true</b><br/>";
+		}
+	}
+}
+
+
+echo "<hr/>";
+
+echo "<p>Solution with 'while' loop</p><br/>";
+
+
+$tests=array(true,"tariq",1.5,true,7,'s',false);
+
+$i=0;
+while ($i < count($tests)){ 
+	
+		if (gettype($tests[$i]) != 'boolean' ) {
+			echo gettype($tests[$i]),"<br/>";
+		}else{
+
+			if ($tests[$i] != true){
+			echo "<b>false</b><br/>";
+			}else {
+			echo "<b>true</b><br/>";
+			}
+		}
+
+	$i++;
+}
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>8 - max </p><br/>";
+
+
+$tests=array(5,4,9,3,1,7,5,8,6);
+
+$i=0;
+
+foreach ($tests as $v) {
+
+	if($v > $i ){
+
+	$i= $v;
+
+	}
+	
+}
+echo $i ;
+
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>9 - Sum </p><br/>";
+
+
+
+$tests=array(5,4,9,3,1,7,5,8,6);
+
+
+$i=0;
+
+foreach ($tests as $v) {
+	$i += $v;
+}
+echo $i ;
+
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>10 - average </p><br/>";
+
+
+$tests=array(6,4,9,3,12,8,7);
+
+
+$i=0;
+
+foreach ($tests as $v) {
+	$i += $v;
+}
+echo $i/count($tests) ;
+
+
+echo "<hr/>";
+
+//===============================
+
+echo "<p>11 - sorting  </p><br/>";
+
+
+$tests=array(6,4,9,3,12,8,7);
+// $fisrt=$tests[0];
+sort($tests); 
+  
+$arrlength = count($tests); 
+for($i = 0; $i < $arrlength; $i++) { 
+    echo $tests[$i]." - ";
+
+    //want to solve the last DASH of 12
+ 
+}
+
+echo "<hr/>";
+
+
+
+
+
+
+
+?>
